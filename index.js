43582c1fe0924faab19168e8ef8f6d312e35effb
786Nurjaman <nurjamanshekh@gmail.com>
2021-11-23 22:36:02 +0530

    Chat

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var socket= require('socket.io')
+require('dotenv').config();
+const port = process.env.PORT||8000
+var app=express()
+
+app.use(express.static('public'));
+var server = app.listen(port,function(){
+    console.log("The server is running : http://localhost:8000/")
+})
+
+
+var io= socket(server);// pass the server under socket method and assign into variable
+io.on('connection',socket=>{ 
+    socket.on("chat",function(data){
+        io.sockets.emit("chat",data)
+    })
+    socket.on("typing",function(data){
+        socket.broadcast.emit("typing",data);
+    })
+})
